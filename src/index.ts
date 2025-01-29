e42300153bc86c4cb2e0d3eeec421306b58138ae
Avaer Kazmer <ultimus@gmail.com>
2025-01-28 18:47:32 -0800

    Update src

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import type { Plugin } from "@elizaos/core";
 import { getOnChainActions } from "./actions";
 import { getWalletProvider } from "./wallet";
 
-const goatPlugin: Plugin = {
+const goatPlugin = {
     name: "[GOAT] Onchain Actions",
     description: "Mode integration plugin",
     providers: [getWalletProvider()],
@@ -10,4 +9,4 @@ const goatPlugin: Plugin = {
     services: [],
     actions: getOnChainActions(),
 };
-export default goatPlugin
+export default goatPlugin;
